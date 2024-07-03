8ecf34ac6fede2ad22b0c128a7bb1d1c056bd9d1
sadneo <haywardeel@gmail.com>
2024-07-02 22:50:23 -0400

    Added editing and deleting todos

--- static/script.js
+++ static/script.js
@@ -6,6 +6,7 @@ let categoryOption = 0;
 let sortOption = 0;
 
 let todos = [];
+let todoItems = [];
 
 function addTodo() {
     const addTodoContent = document.querySelector("#addTodoContent");
@@ -38,6 +39,7 @@ function update() {
     fetch("get").then((response) => {
         response.json().then((value) => {
             todos = value;
+            todoItems = [];
             render();
         });
     });
@@ -45,17 +47,60 @@ function update() {
 
 function render() {
     content.textContent = ""
-
-    for (todo of todos) {
-        const todoNode = todoTemplate.content.cloneNode(true);
-        const todoTime = todoNode.querySelector("#todoTime");
+    for (const [index, todo] of todos.entries()) {
+        const todoItem = todoTemplate.content.cloneNode(true);
+        const todoTime = todoItem.querySelector("#todoTime");
         todoTime.textContent = todo.time.secs_since_epoch;
-        const todoText = todoNode.querySelector("#todoText");
+        const todoText = todoItem.querySelector("#todoText");
         todoText.textContent = todo.content;
 
+        todoItem.querySelector("#todoEditInput").addEventListener("keydown", (event) => {
+            if (event.key === "Enter") {
+                confirmEditTodo(event);
+            }
+        });
+        todoItem.querySelector("#todoEdit").addEventListener("click", editTodo);
+        todoItem.querySelector("#todoDelete").addEventListener("click", deleteTodo);
+
+        todoTime.parentElement.id = index;
+        todoItems[todoTime.parentElement.id] = index;
+
         const content = document.querySelector("#content");
-        content.appendChild(todoNode);
+        content.appendChild(todoItem);
     }
+}
+
+function editTodo(event) {
+    let todo = event.target.parentElement;
+    let todoEditInput = todo.querySelector("#todoEditInput");
+    let todoText = todo.querySelector("#todoText");
+
+    todoEditInput.value = todoText.textContent;
+    todoEditInput.style.display = "initial";
+    todoText.style.display = "none";
+    todoEditInput.focus();
+}
+
+function confirmEditTodo(event) {
+    let todo = event.target.parentElement;
+    let todoEditInput = todo.querySelector("#todoEditInput");
+    let todoText = todo.querySelector("#todoText");
+
+    let index = todoItems[todo.id];
+    fetch(`/edit/${index}/${todoEditInput.value}`, {method:"PATCH"}).then(() => {
+        todoEditInput.style.display = "none";
+        todoText.style.display = "initial";
+        update();
+    });
+}
+
+function deleteTodo(event) {
+    let todo = event.target.parentElement;
+    let index = todoItems[todo.id];
+    fetch(`/delete/${index}`, {method:"DELETE"}).then(() => {
+        todoItems[todo] = null;
+        update();
+    });
 }
 
 update();
