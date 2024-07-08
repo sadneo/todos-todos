37915be5c7e8b06f304e243cb0005a85e9981df4
sadneo <haywardeel@gmail.com>
2024-07-08 18:28:37 -0400

    Homepage displays priority and lets you change it

--- static/script.js
+++ static/script.js
@@ -26,9 +26,7 @@ function addTodo() {
 
 document.querySelector("#addTodo").addEventListener("click", addTodo);
 document.querySelector("#addTodoContent").addEventListener("keydown", (event) => {
-    if (event.key === "Enter") {
-        addTodo();
-    }
+    if (event.key === "Enter") addTodo();
 });
 
 function update() {
@@ -50,24 +48,23 @@ function render() {
         const todoTime = todoItem.querySelector("#todoTime");
         const date = new Date(todo.time.secs_since_epoch * SEC_TO_MILLISEC);
         todoTime.textContent = date.toLocaleString();
+        const todoPriority = todoItem.querySelector("#todoPriority");
+        todoPriority.textContent = todo.priority
+        todoPriority.style["padding-left"] = `${3 - String(todo.priority).length}ch`;
         const todoText = todoItem.querySelector("#todoText");
         todoText.textContent = todo.short;
         const todoDescription = todoItem.querySelector("#todoDescription");
         todoDescription.textContent = todo.description;
 
         todoItem.querySelector("#todoTextInput").addEventListener("keydown", (event) => {
-            if (event.key === "Enter") {
-                confirmEditTodoText(event);
-            }
+            if (event.key === "Enter") confirmEditTodoText(event);
         });
         todoItem.querySelector("#todoEditText").addEventListener("click", editTodoText);
         todoItem.querySelector("#todoDescriptionInput").addEventListener("keydown", (event) => {
-            if (event.key === "Enter") {
-                confirmEditTodoDescription(event);
-            }
+            if (event.key === "Enter") confirmEditTodoDescription(event);
         });
         todoItem.querySelector("#todoEditDescription").addEventListener("click", editTodoDescription);
-        // priority
+        todoItem.querySelector("#todoSetPriority").addEventListener("click", editTodoPriority);
         // tags
         todoItem.querySelector("#todoDelete").addEventListener("click", deleteTodo);
 
@@ -140,6 +137,28 @@ function confirmEditTodoDescription(event) {
     }).then(() => {
         todoDescriptionInput.style.display = "none";
         todoDescription.style.display = "initial";
+        update();
+    });
+}
+
+function editTodoPriority(event) {
+    const todoContainer = event.target.closest("#todoContainer");
+    const priority = Number(prompt("New priority value 0-255", body.priority));
+    if (priority < 0 && priortiy > 255) {
+        alert("Invalid value!");
+        return;
+    }
+    const body = structuredClone(todos[todoContainer.index]);
+    body.priority = priority;
+    body.index = todoContainer.index;
+
+    fetch("/edit", {
+        method: "PATCH",
+        body: JSON.stringify(body),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    }).then(() => {
         update();
     });
 }
